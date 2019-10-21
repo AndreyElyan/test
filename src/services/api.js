4f75718eecdbe49ae11bf8cb40ddb9c9543d660a
andrey <a.elyan.s@gmail.com>
2019-10-21 09:17:17 -0300

    :hammer:

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const BASIC_TOKEN =
   'Basic cmM0TTJDdHFKRmpNSzRRYm92WWlSbXRsWTc5bk1IYzk6a1dodHFwWXdmak5uSE5lYzZBcndjZnRTU0dJUWRQNGU=';
 
 const api = axios.create({
-  baseURL: 'https://abf.homolog.api.somosdx.co',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use(request => {
